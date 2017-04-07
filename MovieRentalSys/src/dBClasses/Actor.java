00fb547810c26afb84128919b46bd7b191dca03d
Lyrene Labor <Lyrene Labor@10.230.37.46>
2017-04-06 20:35:10 -0400

    Actor, Movie and JDBC driver

---
+++ MovieRentalSys/src/dBClasses/Actor.java
@@ -0,0 +1,99 @@
+/**
+ * 
+ */
+package dBClasses;
+
+import java.util.ArrayList;
+
+/**
+ * The Actor class encapsulates the attributes and properties 
+ * of an actor in a movie rental database
+ * @author Lyrene Labor
+ * @version April 2017
+ */
+public class Actor {
+	
+	
+
+	private int actorid;
+	private String name;
+	private String gender;
+	private int age;
+	private ArrayList<Movie> movies;
+	
+	/**
+	 * The Actor constructor initializes all attributes of an actor object
+	 * @param actorid - int value id of the actor
+	 * @param name - string value of the name of the actor
+	 * @param gender - string value of the gender of the actor
+	 * @param age - int value of the age of the actor
+	 */
+	public Actor(int actorid, String name, String gender, int age) {
+		this.actorid = actorid;
+		this.name = name;
+		this.gender = gender;
+		this.age = age;
+	}
+	
+	/**
+	 * getMovies returns the list of movies the actor is starring at
+	 * @return the movies - list of movie objects
+	 */
+	public ArrayList<Movie> getMovies() {
+		return this.movies;
+	}
+	
+	/**
+	 * addMovie adds a movie in the list of movies that the actor is starring
+	 * @param movie - a Movie object
+	 */
+	public void addMovie(Movie movie){
+		this.movies.add(movie);
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Actor [actorid=" + actorid + ", name=" + name + ", gender=" + gender + ", age=" + age + ", movies="
+				+ movies + "]";
+	}
+
+	/**
+	 * getActorid returns the id of the actor
+	 * @return int value
+	 */
+	public int getActorid() {
+		return actorid;
+	}
+	
+	/**
+	 * getName returns the name of the actor
+	 * @return String value
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * getGender returns the gender of the actor
+	 * @return - String value
+	 */
+	public String getGender() {
+		return gender;
+	}
+	
+	/**
+	 * getAge returns the age of the actor
+	 * @return - int value
+	 */
+	public int getAge() {
+		return age;
+	}
+	
+	
+	
+	
+
+}
--- MovieRentalSys/src/dBClasses/Movie.java
+++ MovieRentalSys/src/dBClasses/Movie.java
@@ -3,14 +3,107 @@
  */
 package dBClasses;
 
+import java.util.ArrayList;
+
 /**
- * @author Daniel Bezerra
- *
+ * The Movie class holds the properties and attributes
+ * of a movie object in a movie rental database such as
+ * the title, the category, the number of copies available,
+ * the rating
+ * @author Lyrene Labor
+ * @version April 2017
  */
 public class Movie {
-	//Testing
-	public Movie(){
-		//testing from lyrene laptop
+	
+	//column names
+	private int movieid;
+	private String title;
+	private String category;
+	private int qty;
+	private int rating;
+	private ArrayList<Actor> actorsInMovie;
+	
+	/**
+	 * Contructor initializes all attributes of a movie object
+	 * @param movieid - int value representing the id of the movie in the database
+	 * @param title - the title of the movie
+	 * @param category - the category of the movie
+	 * @param qty - number of copies available of the movie in the database
+	 * @param rating - the rating of the movie
+	 */
+	public Movie(int movieid,String title,String category,int qty,int rating){
+		this.movieid = movieid;
+		this.title = title;
+		this.category = category;
+		this.qty = qty;
+		this.rating = rating;
 	}
+
+	/**
+	 * getActorsInMovie returns the list of actors starring in the movie
+	 * @return the actorsInMovie
+	 */
+	public ArrayList<Actor> getActorsInMovie() {
+		return actorsInMovie;
+	}
+	
+	/**
+	 * addActor adds an actor object to the end of the list of actors of the movie
+	 * @param actor - An actor object
+	 */
+	public void addActor(Actor actor){
+		this.actorsInMovie.add(actor);
+	}
+
+	/**
+	 * getMovieid returns the id value of the movie
+	 * @return int value
+	 */
+	public int getMovieid() {
+		return movieid;
+	}
+
+	/**
+	 * getTitle method returns the title of the book
+	 * @return String value
+	 */
+	public String getTitle() {
+		return title;
+	}
+
+	/**
+	 * getCategory returns the category of the movie
+	 * @return String value
+	 */
+	public String getCategory() {
+		return category;
+	}
+	
+	/**
+	 * getQty returns the number of copies available of the movie in the database
+	 * @return int value
+	 */
+	public int getQty() {
+		return qty;
+	}
+	
+	/**
+	 * getRating returns the rating of the movie
+	 * @return int value
+	 */
+	public int getRating() {
+		return rating;
+	}
+	
+	/**
+	 * toString returns the string representation of the movie's attributes
+	 */
+	@Override
+	public String toString() {
+		return "Movie [movieid=" + movieid + ", title=" + title + ", category=" + category + ", qty=" + qty
+				+ ", rating=" + rating + "]";
+	}
+	
+	
 
 }
